6817b4b584ab876b495fa2805e5ad64e2b79e030
Delcin1 <naklz9@mail.ru>
2025-04-10 12:43:22 +0300

    complete lab2

--- app/src/main/java/com/zhirnovnv/drivenext/ui/home/HomeFragment.kt
+++ app/src/main/java/com/zhirnovnv/drivenext/ui/home/HomeFragment.kt
@@ -1,42 +1,56 @@
 package com.zhirnovnv.drivenext.ui.home
 
 import android.os.Bundle
+import android.text.Editable
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import android.text.TextWatcher
 import com.zhirnovnv.drivenext.databinding.FragmentHomeBinding
+import com.zhirnovnv.drivenext.activities.Car
+import com.zhirnovnv.drivenext.activities.CarAdapter
+import com.zhirnovnv.drivenext.R
+
 
 class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
+    private lateinit var carAdapter: CarAdapter
+    private val carList = listOf(
+        Car("S 500 Sedan", "Mercedes-Benz", "2500Р в день", "A/T", "Бензин", R.drawable.ic_mercbenz),
+        Car("TT Quattro", "Audi", "2000Р в день", "DC/T", "Бензин", R.drawable.ic_audi),
+        Car("Huracan", "Lamborghini", "3500Р в день", "DC/T", "Бензин", R.drawable.ic_lamborghini),
+        Car("Roadster", "Tesla", "3000Р в день", "SS/T", "Электро", R.drawable.ic_tesla)
+    )
 
-    private var _binding: FragmentHomeBinding? = null
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                          savedInstanceState: Bundle?): View {
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+        binding = FragmentHomeBinding.inflate(layoutInflater)
+        val view = binding.root
+        binding.rvCar.layoutManager = LinearLayoutManager(context)
+        carAdapter = CarAdapter(carList)
+        binding.rvCar.adapter = carAdapter
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+        binding.textInputEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable?) {
+                filterCars(s.toString())
+            }
+        })
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        return view
+    }
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+    private fun filterCars(query: String) {
+        val filteredList = if (query.isEmpty()) {
+            carList
+        } else {
+            carList.filter { it.brand.contains(query, ignoreCase = true) }
         }
-        return root
+        carAdapter.updateCars(filteredList)
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
 }
